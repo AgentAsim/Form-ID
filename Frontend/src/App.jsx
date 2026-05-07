cf4d78e211de3c31d047c58347c8aa4de1481efb
SchutzAsim <asimsaifioffical12@gmail.com>
2026-05-07 11:06:25 +0530

    fix login state

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { Login } from './components/Login'
 import { Nav } from './components/Nav'
 import { Home } from './components/Home'
@@ -12,6 +12,41 @@ function App() {
 
   const API_Connect = import.meta.env.VITE_API;
   const access_token = localStorage.getItem('token')
+  const [authorized, setAuthorized] = useState(false)
+
+  const navigate = useNavigate();
+
+
+
+  useEffect(() => {
+    let UserInSession = async () => {
+      let res = await fetch(`${API_Connect}/user/session`, {
+        method: "GET",
+        headers: {
+          'Authorization': `Bearer ${access_token}`,
+          'Content-Type': 'application/json'
+        }
+      });
+
+      let session_res = await res.json();
+
+      if (session_res === true) {
+        navigate("/")
+      }
+      else {
+        navigate("/login")
+      }
+
+      console.log(session_res)
+      
+      return session_res
+    }
+
+    UserInSession();
+
+  }, [])
+
+
 
   const [oldData, setoldData] = useState({ id: "undefine" })
 
@@ -19,9 +54,6 @@ function App() {
     query: ''
   })
   const [searchData, setsearchData] = useState([])
-
-  const [authorized, setAuthorized] = useState(false)
-  console.log(`login State: ${authorized}`)
 
   return (
     <>
@@ -41,8 +73,8 @@ function App() {
           }
         }>
 
-        <Nav />
         <div className={`container`}>
+          <Nav />
           <Routes>
             <Route path='/login' element={<Login />} />
             <Route path="/" element={<Home />} />
