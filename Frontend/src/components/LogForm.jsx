ee13beff2198e9a1ce827a1ba2b0895544920dc7
GeistAsim <asimsaifioffical12@gmail.com>
2026-08-30 11:54:00 +0530

    form done

--- Frontend/src/components/LogForm.jsx
+++ Frontend/src/components/LogForm.jsx
@@ -184,7 +184,7 @@ export const LogForm = () => {
                             
                             <div className="total-amount-box">
                                 <div className="total-amount-label">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                                    <svg style={{color: "#3a6aaf"}} xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
                                         <rect width="20" height="14" x="2" y="5" rx="2"/><line x1="2" x2="22" y1="10" y2="10"/>
                                     </svg>
                                     <span>Total Amount</span>
