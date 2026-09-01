92f247801963820b0d000c7b5f7454e3167ea3cf
GeistAsim <asimsaifioffical12@gmail.com>
2026-09-01 16:28:43 +0530

    move logo to assets

--- Frontend/src/components/Login.jsx
+++ Frontend/src/components/Login.jsx
@@ -2,7 +2,7 @@ import React, { useContext, useState, useEffect } from 'react'
 import { ContainerContext } from '../Context/context'
 import { useNavigate } from 'react-router-dom'
 import { useForm } from 'react-hook-form'
-import logo from '/public/logo.png'
+import logo from '../assets/logo.png'
 
 export const Login = () => {
 
